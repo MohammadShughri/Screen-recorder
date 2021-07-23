3027ae78d8cb0d8ba8e8a1435183cd82db81ee47
MohammadShughri <Mohammadws19@gmail.com>
2021-07-23 17:05:51 +0300

    added screen record function

--- src/index.js
+++ src/index.js
@@ -8,9 +8,14 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 
 const createWindow = () => {
   // Create the browser window.
+  
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true    }
   });
 
   // and load the index.html of the app.
